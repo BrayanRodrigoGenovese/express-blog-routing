22f78a37c027e40daed138a440cce7ff31acb908
Brayan <bittolo.8bit@gmail.com>
2026-03-02 17:52:20 +0100

    adding router to app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const postsRouter = require("./routers/posts");
+
+app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+  console.log(`Server avviato sulla porta ${port}`);
+});
